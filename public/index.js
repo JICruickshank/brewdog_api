5e62b2704eebe2bdff30577cfaed1130f487bc5d
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-11 18:38:33 +0100

    adds makeRequest

--- public/index.js
+++ public/index.js
@@ -1,4 +1,24 @@
+const makeRequest = function(url, callback) {
+  const request = new XMLHttpRequest();
+  request.open("GET", url);
+  request.addEventListener("load", callback);
+  request.send();
+};
+
+const requestComplete = function() {
+  if(this.status !== 200) return;
+  console.log("Testing");
+}
+
+
+
+
+
+
+
 var app = function(){
+  const url = "https://api.punkapi.com/v2/beers";
+  makeRequest(url, requestComplete);
 
 }
 
