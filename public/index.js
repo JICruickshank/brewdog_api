84def365b4cbe424852e6d6d3d01fb48541dbbd1
Jonathan Cruickshank <ikenna1982@hotmail.com>
2018-04-11 19:10:04 +0100

    adds display beers

--- public/index.js
+++ public/index.js
@@ -9,14 +9,30 @@ const requestComplete = function() {
   if(this.status !== 200) return;
   const jsonString = this.responseText;
   const beers = JSON.parse(jsonString);
+  displayBeers(beers);
   debugger;
 }
 
+const createUl = function(id) {
+  const ul = document.createElement("ul");
+}
 
+const createContainer = function(className) {
+  const div = document.createElement("div");
+}
 
+const createLi = function(id, innerText) {
+  const li = document.createElement("li");
+  li.innerText = innerText;
+  const ul = document.getElementById(id);
+  ul.appendChild(li);
+}
 
-
-
+const displayBeers = function(beers) {
+  for(let beer of beers) {
+    createLi("beer-list", beer.name);
+  }
+}
 
 var app = function(){
   const url = "https://api.punkapi.com/v2/beers";
